b4e75693b5639ab23fbeb3b773517339e8e52285
Nicolas Maubourguet <nicolas@kiwixi.com>
2013-08-29 09:40:34 +0200

    add bootstrap modal for creating bucket and key.

--- js/scripts/app.js
+++ js/scripts/app.js
@@ -8,11 +8,54 @@ LDBB.Bucket = Ember.Object.extend({
 //kind of class method.
 LDBB.Model = Ember.Object.extend();
 
-LDBB.BucketsController = Ember.ArrayController.extend();
+LDBB.BucketsController = Ember.ArrayController.extend({
+    addBucket:function(){
+        var name = this.get('newBucketName');
+        var model = LDBB.Bucket.create({'id':name});
+        LDBB.Bucket.createRecord(model);
+    }
+});
+
+
+LDBB.BucketController = Ember.ObjectController.extend({
+    addKey:function(){
+        var name = this.get('newKeyName');
+        //var model = this.get('content');
+        //debugger;
+        var model = LDBB.Key.create({'id':this.get('content.id')+name,"bucketName":this.get('content.id'),"keyName":name,"value":""});
+        console.log(model);
+        LDBB.Key.createRecord(model);
+        var bucket = LDBB.Bucket.find(this.get('content.id'));
+        bucket.get('key_ids').pushObject(name);
+    },
+    deleteBucket:function(){
+        var model = this.get('content');
+        LDBB.Bucket.deleteRecord(model);
+    }
+})
+
+
+LDBB.BucketKeyController = Ember.ObjectController.extend({
+    isEditing:false,
+    doSave:function(){
+        console.log('saving');
+        this.set('isEditing',false);
+        var model = this.get('content');
+        LDBB.Key.updateRecord(model);
+
+    },
+    doEdit:function(){
+        this.set('isEditing',true);
+    },
+    modelObserver:function(){
+        this.set('isEditing',false);
+    }.observes('content')
+
+})
 
 LDBB.Model.reopenClass({
     find:function(url,id,type,key){
-        console.log('find id: ' + id + ' url: ' + url + ' type: ' + type);
+        //console.log('find id: ' + id + ' url: ' + url + ' type: ' + type);
         var collection = Ember.get(type, 'collection');
 
         if (!collection) {
@@ -25,7 +68,7 @@ LDBB.Model.reopenClass({
             foundItem = type.create({id: id});
             $.getJSON(url + "/" + id, function(data) {
                 if (data[key]) {
-                    console.log(data[key])
+
                     foundItem.setProperties(data[key]);
                 }
             });
@@ -39,7 +82,7 @@ LDBB.Model.reopenClass({
     },
 
     findAll: function(url, type, key) {
-        console.log('Model findAll: ' + type);
+        //console.log('Model findAll: ' + type);
         var collection = Ember.get(type, 'collection');
 
         var model = this;
@@ -51,7 +94,7 @@ LDBB.Model.reopenClass({
 
         $.getJSON(url, function(data) {
 
-            console.log(data);
+
             if (data[key]) {
                 $.each(data[key], function(i, row) {
                     var item = model.getObjectById(row.id, type)
@@ -66,6 +109,88 @@ LDBB.Model.reopenClass({
         });
 
         return Ember.get(type, 'collection');
+    },
+    updateRecord: function(url, type, model) {
+        //console.log('update: ' + type + " " + model.get('id'));
+
+        model.set('isSaving', true);
+
+        $.ajax({
+            type: "PUT",
+            url: url,
+            data: JSON.stringify(model),
+            success: function(res, status, xhr) {
+                if (res.id) {
+                    model.set('isSaving', false);
+                    model.setProperties(res);
+                } else {
+                    model.set('isError', true);
+                }
+            },
+            error: function(xhr, status, err) { model.set('isError', false);  }
+        });
+    },
+    createRecord: function(url, type, model) {
+        console.log('create: ' + type + " " + model.get('id'));
+        //var model = this;
+
+        var collection = Ember.get(type, 'collection');
+
+
+
+        if (!collection) {
+            collection = Ember.A();
+            Ember.set(type, 'collection', collection);
+        }
+
+        model.set('isSaving', true);
+        console.log(JSON.stringify(model));
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: JSON.stringify(model),
+            success: function(res, status, xhr) {
+                if (res.id) {
+                    model.set('isSaving', false);
+                    model.setProperties(res);
+                    collection.pushObject(model);
+                } else {
+                    model.set('isError', true);
+                }
+            },
+            error: function(xhr, status, err) { model.set('isError', false);  }
+        });
+    },
+    deleteRecord: function(url, type, model) {
+        //console.log('delete: ' + type + " " + model.get('id'));
+        //var model = this;
+
+        var collection = Ember.get(type, 'collection');
+
+
+
+        if (!collection) {
+            collection = Ember.A();
+            Ember.set(type, 'collection', collection);
+        }
+
+        model.set('isSaving', true);
+
+        $.ajax({
+            type: "DEL",
+            url: url,
+            data: JSON.stringify(model),
+            success: function(res, status, xhr) {
+                if (res.id) {
+                    model.set('isSaving', false);
+                    model.setProperties(res);
+                    collection.removeObject(model);
+                } else {
+                    model.set('isError', true);
+                }
+            },
+            error: function(xhr, status, err) { model.set('isError', false);  }
+        });
     },
 
 
@@ -104,6 +229,12 @@ LDBB.Bucket = LDBB.Bucket.reopenClass({
     },
     findAll:function(){
         return LDBB.Model.findAll('/json/buckets',LDBB.Bucket,'buckets');
+    },
+    createRecord: function(model) {
+        LDBB.Model.createRecord("/json/buckets/", LDBB.Bucket,model);
+    },
+    deleteRecord: function(model) {
+        LDBB.Model.deleteRecord("/json/buckets/", LDBB.Bucket,model);
     }
 });
 
@@ -114,7 +245,18 @@ LDBB.Key = LDBB.Key.reopenClass({
     },
     findAll:function(bucketid){
         return LDBB.Model.find('/json/buckets/' + bucketid ,LDBB.Key,'bucket');
+    },
+    updateRecord: function(model) {
+        LDBB.Model.updateRecord("/json/buckets/" + model.get('bucketName') + "/key/" + model.get('keyName'), LDBB.Key, model);
+    },
+    createRecord: function(model) {
+        LDBB.Model.createRecord("/json/buckets/" + model.get('bucketName') + "/key/", LDBB.Key,model);
+    },
+    deleteRecord: function(model) {
+        //???? url is unkown
+        LDBB.Model.deleteRecord("/json/buckets/", LDBB.Key,model);
     }
+
 });
 
 //ember router
@@ -122,6 +264,7 @@ LDBB.Router.map(function() {
 	this.resource("buckets", {path: "/"}, function() {
 		this.resource("bucket", {path: "/bucket/:bucket_id"}, function() {
 			this.route("key", {path: "/key/:key_id"}); });
+            //this.route("edit", {path: "/edit/:key_id"}); });
 	});	
 });
 
